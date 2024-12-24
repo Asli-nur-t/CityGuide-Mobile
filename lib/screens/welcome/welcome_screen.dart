7585aff4084d4babccdcad7a3eb8bd98f64a15ad
Aslınur Topcu <aslinurtopcu@Aslnur-MacBook-Air.local>
2024-12-24 03:45:40 +0300

    welcome page changes

--- lib/screens/welcome/welcome_screen.dart
+++ lib/screens/welcome/welcome_screen.dart
@@ -1,6 +1,7 @@
 import '../../constants.dart';
 import '../signinOrSignUp/signin_or_signup_screen.dart';
 import 'package:flutter/material.dart';
+import 'dart:ui'; // For ImageFilter.blur
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
@@ -8,69 +9,98 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: kBackgroundColor,
-      body: SafeArea(
-        child: Column(
-          children: [
-            const Spacer(flex: 2),
-            Image.asset("assets/images/welcome_image.png"),
-            const Spacer(flex: 1),
-            Text(
-              "Welcome to our  \ntravel app",
-              textAlign: TextAlign.center,
-              style: Theme.of(context)
-                  .textTheme
-                  .headlineSmall!
-                  .copyWith(fontWeight: FontWeight.bold),
+      body: Stack(
+        children: [
+          // Resmi tam ekran olarak ayarla
+          Positioned.fill(
+            child: Image.asset(
+              "assets/images/welcome_image.png",
+              fit: BoxFit.cover, // Ekranı tamamen kaplamasını sağlar
             ),
-            const Spacer(),
-            Text(
-              "CityGuide ile en popüler yerleri, gizli köşeleri \n ve en iyi restoranları keşfedin.",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Theme.of(context)
-                    .textTheme
-                    .bodyLarge!
-                    .color!
-                    .withOpacity(0.64),
-              ),
-            ),
-            const Spacer(flex: 3),
-            FittedBox(
-              child: TextButton(
-                  onPressed: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const SigninOrSignupScreen(),
+          ),
+          // İçeriği ekle
+          Align(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width *
+                          0.9, // Ekranın %90'ını kaplayacak şekilde ayarlandı
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10),
+                      color: Colors.black.withOpacity(0.5),
+                      child: Text(
+                        "Welcome to our \ntravel app",
+                        textAlign: TextAlign.center,
+                        style:
+                            Theme.of(context).textTheme.headlineSmall!.copyWith(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20), // Boşluk eklemek için
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width *
+                          0.9, // Ekranın %90'ını kaplayacak şekilde ayarlandı
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10),
+                      color: Colors.black.withOpacity(0.5),
+                      child: Text(
+                        "CityGuide ile en popüler yerleri, gizli köşeleri \n ve en iyi restoranları keşfedin.",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.9),
                         ),
                       ),
-                  child: Row(
-                    children: [
-                      Text(
-                        "Skip",
-                        style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                              color: Theme.of(context)
-                                  .textTheme
-                                  .bodyLarge!
-                                  .color!
-                                  .withOpacity(0.8),
-                            ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 40),
+                FittedBox(
+                  child: TextButton(
+                    onPressed: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const SigninOrSignupScreen(),
                       ),
-                      const SizedBox(width: kDefaultPadding / 4),
-                      Icon(
-                        Icons.arrow_forward_ios,
-                        size: 16,
-                        color: Theme.of(context)
-                            .textTheme
-                            .bodyLarge!
-                            .color!
-                            .withOpacity(0.8),
-                      )
-                    ],
-                  )),
-            )
-          ],
-        ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Skip",
+                          style:
+                              Theme.of(context).textTheme.bodyLarge!.copyWith(
+                                    color: Colors.white.withOpacity(0.8),
+                                  ),
+                        ),
+                        const SizedBox(width: 10),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          size: 16,
+                          color: Colors.white.withOpacity(0.8),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
