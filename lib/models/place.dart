d5ecd257a039694eb5aa57e4009a3715be691106
Aslınur Topcu <aslinurtopcu@Aslnur-MacBook-Air.local>
2024-12-30 13:09:15 +0300

    Added Hive integration and local database support for favorites

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -1,50 +1,91 @@
+// // Created by https://github.com/Asli-nur-t
+// // Date: 2024-12-30
+
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+// class Place {
+//   final String id;
+//   final String name;
+//   final double rating;
+//   final LatLng location;
+//   final String category;
+//   final String address; // Yeni alan
+//   final String description; // Opsiyonel açıklama alanı
+
+//   Place({
+//     required this.id,
+//     required this.name,
+//     required this.rating,
+//     required this.location,
+//     required this.category,
+//     required this.address,
+//     this.description = '', // Varsayılan olarak boş
+//   });
+
+//   Map<String, dynamic> toMap() {
+//     return {
+//       'id': id,
+//       'name': name,
+//       'rating': rating,
+//       'latitude': location.latitude,
+//       'longitude': location.longitude,
+//       'category': category,
+//       'address': address,
+//       'description': description,
+//     };
+//   }
+
+//   factory Place.fromMap(Map<String, dynamic> map) {
+//     return Place(
+//       id: map['place_id'],
+//       name: map['name'],
+//       rating: map['rating']?.toDouble() ?? 0.0,
+//       location: LatLng(map['geometry']['location']['lat'],
+//           map['geometry']['location']['lng']),
+//       category: map['types']?.join(', ') ?? '',
+//       address: map['vicinity'] ?? 'No address available',
+//       description: map['description'] ?? '',
+//     );
+//   }
+// }
+
 // Created by https://github.com/Asli-nur-t
 // Date: 2024-12-30
 
+import 'package:hive/hive.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+part 'place.g.dart'; // Hive adapteri için oluşturulacak dosya
+
+@HiveType(typeId: 0) // Benzersiz bir typeId belirleyin
 class Place {
+  @HiveField(0)
   final String id;
+  @HiveField(1)
   final String name;
+  @HiveField(2)
   final double rating;
-  final LatLng location;
+  @HiveField(3)
+  final double latitude;
+  @HiveField(4)
+  final double longitude;
+  @HiveField(5)
   final String category;
-  final String address; // Yeni alan
-  final String description; // Opsiyonel açıklama alanı
+  @HiveField(6)
+  final String address;
+  @HiveField(7)
+  final String description;
 
   Place({
     required this.id,
     required this.name,
     required this.rating,
-    required this.location,
+    required this.latitude,
+    required this.longitude,
     required this.category,
     required this.address,
-    this.description = '', // Varsayılan olarak boş
+    this.description = '',
   });
 
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'name': name,
-      'rating': rating,
-      'latitude': location.latitude,
-      'longitude': location.longitude,
-      'category': category,
-      'address': address,
-      'description': description,
-    };
-  }
-
-  factory Place.fromMap(Map<String, dynamic> map) {
-    return Place(
-      id: map['place_id'],
-      name: map['name'],
-      rating: map['rating']?.toDouble() ?? 0.0,
-      location: LatLng(map['geometry']['location']['lat'],
-          map['geometry']['location']['lng']),
-      category: map['types']?.join(', ') ?? '',
-      address: map['vicinity'] ?? 'No address available',
-      description: map['description'] ?? '',
-    );
-  }
+  LatLng get location => LatLng(latitude, longitude);
 }
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,53 +1,3 @@
-// import 'package:dio/dio.dart';
-// import 'package:google_maps_flutter/google_maps_flutter.dart';
-// import '../models/place.dart';
-
-// // class ApiService {
-// //   final Dio _dio = Dio();
-// //   final String apiKey = 'AIzaSyCbXJ1tIrQcHhpdUAl0gBiNnhwZyu_wgBg';
-
-// //   Future<List<Place>> fetchPlaces(LatLng userLocation, String type) async {
-// //     final String url =
-// //         'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${userLocation.latitude},${userLocation.longitude}&radius=5000&type=$type&key=$apiKey';
-
-// //     try {
-// //       final response = await _dio.get(url);
-// //       final List data = response.data['results'];
-// //       print('Gelen Veri: ${response.data}');
-
-// //       return data.map<Place>((place) => Place.fromMap(place)).toList();
-// //     } catch (e) {
-// //       print('API Hatası: $e');
-// //       return [];
-// //     }
-// //   }
-// // }
-// import 'package:dio/dio.dart';
-// import 'package:chucker_flutter/chucker_flutter.dart';
-
-// class ApiService {
-//   final Dio _dio = Dio();
-
-//   ApiService() {
-//     // Chucker interceptor'ı ekle
-//     _dio.interceptors.add(ChuckerDioInterceptor());
-//   }
-
-//   Future<List> fetchPlaces(LatLng userLocation, String type) async {
-//     final String apiKey = 'AIzaSyCbXJ1tIrQcHhpdUAl0gBiNnhwZyu_wgBg';
-//     final String url =
-//         'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${userLocation.latitude},${userLocation.longitude}&radius=5000&type=$type&key=$apiKey';
-
-//     try {
-//       final response = await _dio.get(url);
-//       print('API Response: ${response.data}');
-//       return response.data['results'];
-//     } catch (e) {
-//       print('API Error: $e');
-//       return [];
-//     }
-//   }
-// }
 import 'package:chucker_flutter/chucker_flutter.dart';
 import 'package:cityguidemob/models/place.dart';
 import 'package:dio/dio.dart';
@@ -74,8 +24,8 @@ class ApiService {
         id: data['place_id'],
         name: data['name'],
         rating: data['rating']?.toDouble() ?? 0.0,
-        location: LatLng(data['geometry']['location']['lat'],
-            data['geometry']['location']['lng']),
+        latitude: data['geometry']['location']['lat'] ?? 0.0,
+        longitude: data['geometry']['location']['lng'] ?? 0.0,
         category: data['types']?.join(', ') ?? '',
         address: data['formatted_address'] ?? 'No address available',
         description: data['editorial_summary']?['overview'] ?? '',
@@ -85,7 +35,6 @@ class ApiService {
     }
   }
 
-  /// Fetch places near a specific location
   Future<List<Place>> fetchPlaces(LatLng userLocation, String type) async {
     const String apiKey = 'AIzaSyCbXJ1tIrQcHhpdUAl0gBiNnhwZyu_wgBg';
     final String url =
@@ -95,7 +44,18 @@ class ApiService {
       final response = await _dio.get(url);
       final List<dynamic> results = response.data['results'];
 
-      return results.map((place) => Place.fromMap(place)).toList();
+      return results.map((place) {
+        return Place(
+          id: place['place_id'],
+          name: place['name'],
+          rating: place['rating']?.toDouble() ?? 0.0,
+          latitude: place['geometry']['location']['lat'] ?? 0.0,
+          longitude: place['geometry']['location']['lng'] ?? 0.0,
+          category: place['types']?.join(', ') ?? '',
+          address: place['vicinity'] ?? 'No address available',
+          description: place['editorial_summary']?['overview'] ?? '',
+        );
+      }).toList();
     } catch (e) {
       throw Exception('Failed to fetch places: $e');
     }
